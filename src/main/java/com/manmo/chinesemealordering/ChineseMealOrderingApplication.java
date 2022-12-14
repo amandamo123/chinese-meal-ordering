a537e0373560a46c01d6239a46752f0a6a88b067
manmo <man.mo@thoughtworks.com>
2022-12-14 21:19:24 +0800

    feat: generate project skeleton

---
+++ src/main/java/com/manmo/chinesemealordering/ChineseMealOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.manmo.chinesemealordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChineseMealOrderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChineseMealOrderingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manmo/chinesemealordering/ChineseMealOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manmo.chinesemealordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChineseMealOrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
